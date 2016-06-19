e861c9538ff3d0ee19a6510c2fc80c289e777d6d
TheSimpleMango <mingda.he@gmail.com>
2016-06-19 14:15:03 -0700

    dasfasdfa

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+
+	JFrame frame;
+	JPanel panel;
+	JButton login, create, exit;
+	
+	public static void main(String[] args){
+		
+		new Main();
+		
+		
+	}
+	
+	public Main(){
+		
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		panel = new JPanel();
+		frame.add(panel);
+		
+		switchGUI(0);
+		
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+	
+	public void switchGUI(int gui){
+		
+		panel.removeAll();
+		
+		switch(gui){
+		case 0:
+			login = new JButton("Login");
+			panel.add(login);
+			create = new JButton("Create new account");
+			panel.add(create);
+			exit = new JButton("Exit");
+			panel.add(exit);
+		}
+		
+	}
+	
+}
